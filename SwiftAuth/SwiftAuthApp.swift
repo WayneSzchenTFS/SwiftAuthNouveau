9104553d3d99df6687c8eb3978c1142e80e161db
waynechen <szchen@tfs.ca>
2023-05-03 10:40:16 -0400

    Initial Commit

---
+++ SwiftAuth/SwiftAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAuthApp.swift
+//  SwiftAuth
+//
+//  Created by Wayne Chen on 2023-05-03.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
